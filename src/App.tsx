4ff8129fda5b24295e6b590bae0376c562070c66
Cristian Saguil <cristian.saguil@gmail.com>
2023-10-08 14:09:11 -0400

    add loading state

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import './App.css';
 
 const App = () => {
     const [planets, setPlanets] = useState<Array<Planet>>([]);
+    const [isLoading, setIsLoading] = useState(true);
     const [selectedIdx, setSelectedIdx] = useState(-1);
 
     useEffect(() => {
@@ -13,6 +14,7 @@ const App = () => {
             if ('results' in data) {
                 setPlanets(data.results);
             }
+            setIsLoading(false);
         }
         getPlanets();
     }, []);
@@ -20,32 +22,61 @@ const App = () => {
         <div className="container">
             <div className="table-container">
                 <h1 className="title">Star Wars Planet Explorer</h1>
-                {planets.length > 0 && (
-                    <table className="planet-table">
-                        <thead className="planet-table-header">
-                        <tr>
-                            <th className="planet-table-cell">Planet Name</th>
-                            <th className="planet-table-cell">Climate</th>
-                            <th className="planet-table-cell">Diameter</th>
-                            <th className="planet-table-cell">Population</th>
-                            <th className="planet-table-cell">Terrain</th>
-                            <th className="planet-table-cell">Rotation Period</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {planets.map((planet, index) => (
-                            <tr key={index} className="planet-table-row">
-                                <td className="planet-table-cell">{planet.name}</td>
-                                <td className="planet-table-cell">{planet.climate}</td>
-                                <td className="planet-table-cell">{planet.diameter}</td>
-                                <td className="planet-table-cell">{planet.population}</td>
-                                <td className="planet-table-cell">{planet.terrain}</td>
-                                <td className="planet-table-cell">{planet.rotation_period}</td>
-                            </tr>
-                        ))}
-                        </tbody>
-                    </table>
-                )}
+                {isLoading
+                    ? 'Loading...'
+                    : planets.length > 0 && (
+                          <table className="planet-table">
+                              <thead className="planet-table-header">
+                                  <tr>
+                                      <th className="planet-table-cell">
+                                          Planet Name
+                                      </th>
+                                      <th className="planet-table-cell">
+                                          Climate
+                                      </th>
+                                      <th className="planet-table-cell">
+                                          Diameter
+                                      </th>
+                                      <th className="planet-table-cell">
+                                          Population
+                                      </th>
+                                      <th className="planet-table-cell">
+                                          Terrain
+                                      </th>
+                                      <th className="planet-table-cell">
+                                          Rotation Period
+                                      </th>
+                                  </tr>
+                              </thead>
+                              <tbody>
+                                  {planets.map((planet, index) => (
+                                      <tr
+                                          key={index}
+                                          className="planet-table-row"
+                                      >
+                                          <td className="planet-table-cell">
+                                              {planet.name}
+                                          </td>
+                                          <td className="planet-table-cell">
+                                              {planet.climate}
+                                          </td>
+                                          <td className="planet-table-cell">
+                                              {planet.diameter}
+                                          </td>
+                                          <td className="planet-table-cell">
+                                              {planet.population}
+                                          </td>
+                                          <td className="planet-table-cell">
+                                              {planet.terrain}
+                                          </td>
+                                          <td className="planet-table-cell">
+                                              {planet.rotation_period}
+                                          </td>
+                                      </tr>
+                                  ))}
+                              </tbody>
+                          </table>
+                      )}
             </div>
         </div>
     );
